85f41a7d35d11dbd1323ee3198c1f77933751dff
jared Higgins <hjared10@yahoo.com>
2019-12-18 20:07:37 -0600

    added arrays to the script

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 function generate(){
-    let com = prompt("please enter a Number", "10");
+    var com = prompt("please enter a Number", "10");
 
-let letters = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890!@#$%^&*()";
+    var low = confirm("lower case?")
+    var upper = confirm("upper case characters?")
+    var special = confirm("special characters?")
+    var num = confirm("numbers?")
+
+console.log(low)
+console.log(upper)
+console.log(special)
+console.log(num)
+
+var letters = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890!@#$%^&*()";
+
+var lowc = "qwertyuioplkjhgfdsazxcvbnm"
+
+var upc = "QWERTYUIOPLKJHGFDSAZXCVBNM"
+
+var spec = "!@#$%^&*()"
+
+var numc = "1234567890"
+
+var total = ""
+
+var passw = ""
+
+if (low="true") {
+    passw = passw + lowc
+    }
+
+if (upper="true") {
+   passw = passw + upc
+    }
+
+if (special="true") {
+    passw = passw + spec
+    }
+
+if (num="true") {
+    passw = passw + numc
+    }
+
+console.log(numc)
 
 var pass = "";
 
 for(var i = 0; i<=com; i++){
-    pass = pass + letters.charAt(Math.floor(Math.random() * Math.floor(letters.length - 1)))
+    pass = pass + passw.charAt(Math.floor(Math.random() * Math.floor(passw.length - 1)))
 }
 
-document.getElementById("display").value=pass;
+console.log(pass)
 
+document.getElementById("display").value=pass;
 
 }
 
 function copypass () {
     document.getElementById("display").select();
     document.execCommand("copy");
-    alert("Password Copied!");
+    alert("Password Copied!")
 }
